95e37b0674f2c36b17bac9af6c8565495c15c271
hpaelike <hpaelike@marum.de>
2022-12-24 12:28:27 +0100

    ...

--- Tests/PangaeaClientCLITests/PangaeaClientCLITests.swift
+++ Tests/PangaeaClientCLITests/PangaeaClientCLITests.swift
@@ -8,9 +8,9 @@ final class PangaeaClientCLITests: XCTestCase {
       // results.
 
       // Some of the APIs that we use below are available in macOS 10.13 and above.
-      guard #available(macOS 10.13, *) else {
-        return
-      }
+      //guard #available(macOS 10.13, *) else {
+      //  return
+      //}
 
       // Mac Catalyst won't have `Process`, but it is supported for executables.
       #if !targetEnvironment(macCatalyst)
@@ -31,6 +31,7 @@ final class PangaeaClientCLITests: XCTestCase {
 
       //XCTAssertEqual(output, "Hello, world!\n")
       #endif
+    
   }
 
   /// Returns path to the built products directory.
